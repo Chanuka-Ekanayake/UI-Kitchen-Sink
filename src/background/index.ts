2c0aca35c5633f5c28698287fbc9d468cf97538e
Chanuka Ekanayake <chanukasamajith@gmail.com>
2026-03-30 02:08:59 +0530

    install dependancies

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,9 +1,10 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Extension installed');
-  
+
   if (chrome.sidePanel) {
     chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(console.error);
   }
+
 });
 
-export {};
+export { };
